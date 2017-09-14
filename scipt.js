faa7cd91805d3a45eb0167f516926b6ff689222e
Megan Penn <penn.meg@gmail.com>
2017-09-14 16:41:55 -0400

    final

--- scipt.js
+++ scipt.js
@@ -1,1 +1,15 @@
+window.onload = function(e){
+    console.log("== window ready! ==");
+    setTimeout(onTimerElapsed, 3000);
+}
 
+var modal = document.getElementById('myModal');
+var span = document.getElementsByClassName("close")[0];
+
+function onTimerElapsed() {
+        modal.style.display = "block";
+};
+
+$('span').click( function(){
+  modal.style.display = "none";
+})
